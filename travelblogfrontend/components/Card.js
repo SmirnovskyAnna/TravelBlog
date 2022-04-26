faaa08e652c3a1dea488b98f95703549e4cac37f
SmirnovskyAnna <smirnova@appbooster.ru>
2022-04-26 16:29:33 +0300

    styled components

--- travelblogfrontend/components/Card.js
+++ travelblogfrontend/components/Card.js
@@ -1,6 +1,37 @@
-const Card = () => {
+import { urlFor } from '../lib/sanity'
+import Tag from '../components/Tag'
+
+const Card = ({ post }) => {
   return (
-    <div>Card</div>
+    <div className="card-container"> 
+      <h2>{post.title}</h2>
+      <p>Published on: {new Date(post.publishedAt).toDateString()}</p>
+      <img 
+        className="main-image"
+        alt={post.title + 'image'}
+        src={urlFor(post.mainImage)}
+      />
+
+      <hr />
+
+      <div className="info-container">
+        <p>Posted by: {post.username}</p>
+        <img 
+          className="avatar"
+          alt={post.username + ' avatar'}
+          src={urlFor(post.authorImage)}
+        />
+      </div>
+
+      <div className="tag-container">
+        {post.categories.map((category) => (
+          <>
+            {category && <Tag key={category} title={category?.title} />}
+          </>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
